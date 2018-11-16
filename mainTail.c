74dbb2e5986e6f546edd60932500617ce33c1a0d
dbulkowski95 <d.bulkowski95@gmail.com>
2018-11-16 14:58:25 +0100

    Tail without list and errors

--- mainTail.c
+++ mainTail.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tail.h"
-
  int main (int argc, const char *argv[])
 {
 	if (argc != 2)
--- tail.c
+++ tail.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 #include "tail.h"
 
-void tail(const int value_of_argv_one)
+void tail( const size_t argv_size)
 {
-	char *charTab = (char*)malloc(sizeof(char) * value_of_argv_one);
-	char c = (char)getchar();
+	char *charTab = (char*)malloc(sizeof(char) * argv_size);
 	size_t counter = 0;
+	char c = (char)getchar();
 	while (c != EOF)
 	{
 		charTab[counter] = c;
-		counter = ((counter + 1) == value_of_argv_one) ?  0 : (counter + 1);
+		counter = ((size_t)(counter + 1) == argv_size) ?  0 : (counter + 1);
 		c = getchar();
 	}
-	for (size_t i = 0; i < value_of_argv_one; i++)
+	for (size_t i = 0; i < argv_size; i++)
 	{
 		printf("%c", charTab[counter]);
-		counter = ((counter + 1) == value_of_argv_one) ?  0 : (counter + 1);
+		counter = ((counter + 1) == argv_size) ?  0 : (counter + 1);
 	}
 	printf("\n");
 	free(charTab);
 }
+
